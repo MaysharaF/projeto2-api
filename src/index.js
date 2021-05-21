14436e62dad0b46d0f9811ab9b06e137d25c7427
MaysharaF <mayshara@alunos.utfpr.edu.br>
2021-05-20 23:24:07 -0300

    Fazendo conexão com heroku

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ require("dotenv").config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
