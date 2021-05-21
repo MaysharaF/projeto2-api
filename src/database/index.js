d08d80b91ed7e4ab50c2045764d3a8d79ba38519
MaysharaF <mayshara@alunos.utfpr.edu.br>
2021-05-20 22:14:48 -0300

    conexão com banco

---
+++ src/database/index.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+  process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
