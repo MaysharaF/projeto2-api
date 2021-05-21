5c5ff12532a62bdb41f438ff117ca523919b90a9
MaysharaF <mayshara@alunos.utfpr.edu.br>
2021-05-20 23:27:38 -0300

    Configurando porta heroku

--- src/index.js
+++ src/index.js
@@ -14,4 +14,4 @@ app.use(bodyParser.urlencoded({ extended: false }));
 require('./controllers/authController')(app);
 require('./controllers/projectController')(app);
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
